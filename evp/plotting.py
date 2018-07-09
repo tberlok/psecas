b09a4148c0c6153e78b70ee74224ef2fac4b9bf2
Thomas Berlok <tberlok@gmail.com>
2018-07-09 18:12:11 +0200

    Added a write_athena function

--- evp/plotting.py
+++ evp/plotting.py
@@ -38,3 +38,50 @@ def load_system(filename):
     import pickle
     system = pickle.load(open(filename, 'rb'))
     return system
+
+
+def write_athena(system, Nz, Lz, path=None):
+    """
+    Interpolate theory onto grid in Athena
+    """
+    import numpy as np
+
+    # Grid points where Athena is defined (improve this!)
+    dz = Lz/Nz
+    z = np.arange(dz/2, Nz*dz, dz)
+    znodes = np.arange(0., (Nz+1)*dz, dz)
+
+    grid = system.grid
+    result = system.result
+
+    if path is None:
+        path = './'
+
+    # Calculate and store imaginary part
+    perturb = []
+    for key in system.variables:
+        if key != 'dA':
+            y = np.hstack([grid.interpolate(z, result[key].imag), 0.0])
+            perturb.append(y)
+
+    if 'dA' in system.variables:
+        znodes = np.arange(0., (Nz+1)*dz, dz)
+        perturb.append(grid.interpolate(znodes, result['dA'].imag))
+
+    perturb = np.transpose(perturb)
+    np.savetxt(path + 'imagPerturbations{}.txt'.format(Nz), perturb,
+               delimiter="\t", newline="\n", fmt="%1.16e")
+
+    # Calculate and store real part
+    perturb = []
+    for key in system.variables:
+        if key != 'dA':
+            y = np.hstack([grid.interpolate(z, result[key].real), 0.0])
+            perturb.append(y)
+
+    if 'dA' in system.variables:
+        perturb.append(grid.interpolate(znodes, result['dA'].real))
+
+    perturb = np.transpose(perturb)
+    np.savetxt(path + 'realPerturbations{}.txt'.format(Nz), perturb,
+               delimiter="\t", newline="\n", fmt="%1.16e")

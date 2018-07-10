bea666939bebb813777994ed03a423b556f210b6
Thomas Berlok <tberlok@gmail.com>
2018-07-10 15:04:21 +0200

    Generate input for Athena

--- examples/athena.py
+++ examples/athena.py
@@ -1,22 +1,26 @@
 import numpy as np
-from evp import Solver, FourierGrid, write_athena
+from evp import Solver, FourierGrid, write_athena, save_system
 from evp.systems.kh_uniform import KelvinHelmholtzUniform
 
 # Set up a grid
 grid = FourierGrid(N=64, zmin=0, zmax=2)
 
 # Set up the system of equations
-system = KelvinHelmholtzUniform(grid, beta=1e4, nu=1e-2, kx=2*np.pi)
+kx = 4.627762711864407
+# kx = 2*np.pi
+system = KelvinHelmholtzUniform(grid, beta=1e5, nu=1e-2, kx=kx)
 
 # Set up a solver
 solver = Solver(grid, system)
 
 # Iteratively solve
 Ns = np.hstack((np.arange(1, 5)*32, np.arange(3, 12)*64))
-solver.iterate_solver(Ns, verbose=True)
+solver.iterate_solver(Ns, verbose=True, tol=1e-10)
 
 # Write files for loading into Athena
-write_athena(system, Nz=256, Lz=1.0)
+write_athena(system, Nz=256, Lz=2.0)
 
 # Write directly to the Athena directory
-write_athena(system, Nz=256, Lz=1.0, path='/Users/berlok/codes/athena/bin/')
+write_athena(system, Nz=256, Lz=2.0, path='/Users/berlok/codes/athena/bin/')
+
+save_system(system, '/Users/berlok/codes/athena/bin/kh-visc.p')

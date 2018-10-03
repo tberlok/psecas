98f88a9f72620e550d4d44dca65502300c129657
Thomas Berlok <tberlok@gmail.com>
2018-10-03 21:08:56 +0200

    Updated to work with C instead of L

--- examples/accretion-disks/channel.py
+++ examples/accretion-disks/channel.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from evp import Solver, ChebyshevRationalGrid
 from evp.systems.channel import Channel
 
-grid = ChebyshevRationalGrid(N=199, L=1)
+grid = ChebyshevRationalGrid(N=199, z='r')
 system = Channel(grid)
 
 ch = Solver(grid, system)
@@ -26,7 +26,7 @@ modes = 3
 fig, axes = plt.subplots(num=1, ncols=modes, sharey=True)
 for mode in range(modes):
     Ns = np.hstack((np.arange(1, 6) * 32, np.arange(2, 12) * 64))
-    omega, vec, err = ch.iterate_solver(Ns, mode=mode, verbose=True)
+    omega, vec, err = ch.iterate_solver(Ns, mode=mode, tol=1e-8, verbose=True)
     phi = np.arctan(vec[2].imag / vec[2].real)
     ch.keep_result(omega, vec * np.exp(-1j * phi), mode=mode)
     axes[mode].set_title(

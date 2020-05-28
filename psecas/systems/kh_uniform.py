78a58066cd04978c8e03205c445c48bb50d7feac
Thomas Berlok <tberlok@gmail.com>
2020-05-28 19:00:51 +0200

    Change to doc-string

--- psecas/systems/kh_uniform.py
+++ psecas/systems/kh_uniform.py
@@ -7,9 +7,9 @@ class KelvinHelmholtzUniform:
 
        More details about this specific setup can be found in
 
-       Berlok, T. & Pfrommer, C. (2019). *On the Kelvin-Helmholtz instability with
-       smooth initial conditions – Linear theory and simulations*,
-       [MNRAS, 485, 908](https://academic.oup.com/mnras/advance-article-abstract/doi/10.1093/mnras/stz379/5308845)
+       Berlok, T. & Pfrommer, C. (2019). *On the Kelvin-Helmholtz instability 
+       with smooth initial conditions – Linear theory and simulations*, MNRAS,
+       485, 908
 
        Another reference for the KHI with anisotric viscosity is
 
